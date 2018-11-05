c0ddfa81c4f2b118508738c0b769851e6988f319
Michael Blatchford <mblatchford@mac.com>
2018-11-05 11:08:53 -0500

    Added connection info to allow node to talk with db

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-console.log("hello World");
-console.log("hello World");
-console.log("hello smallWorld");
-console.log("hello World");
+const pgp = require('pg-promise')();
+
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'todo-node-app-db'
+    //not used for our use locally but will be required when deployed
+    // user: 'user-name',
+    // password: 'user-password'
+});
+
+
+db.any('select * from todos')
+    .then(results => {
+        console.log(results)
+    })    
